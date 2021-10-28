74aba4aa29ca2d158c8f3b26540f3f2b7197eb3a
Gurol Bilgin <gurolbilgin@gmail.com>
2021-10-27 18:32:43 -0600

    first commit

---
+++ app/admin.py
@@ -0,0 +1,19 @@
+from django.contrib import admin
+from .models import Todo
+# Register your models here.
+
+
+class TodoAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "user",
+        "status",
+        'priority',
+        'updated_date',
+        'created_date',
+    )
+    list_filter = ("user", )
+    search_fields = ("title__startswith", )
+
+
+admin.site.register(Todo, TodoAdmin)
